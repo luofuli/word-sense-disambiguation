4251d47380da4a6836b6d26aea9b52f7a8369ae4
Fuli Luo <luofuli@pku.edu.cn>
2018-12-21 16:42:25 +0800

    Update path.py

--- utils/path.py
+++ utils/path.py
@@ -47,8 +47,8 @@ class WSD_path(object):
         self.MFS_PATH = _ALL_WORDS_BASE_PATH + 'Output_Systems_ALL/MFS_{0}.key'
         self.WNFS_PATH = _ALL_WORDS_BASE_PATH + 'Output_Systems_ALL/WNFirstsense.key'
 
-        self.WORDNET_PATH = '../data/nltk_data/corpora/wordnet'  # version 3.0
-        self.GLOVE_VECTOR = '../data/glove.42B.300d.txt'  # todo: change to yours word2vec path
+        self.WORDNET_PATH = '../data/nltk_data/corpora/wordnet'  # todo: change to your wordnet database (version 3.0) path
+        self.GLOVE_VECTOR = '../data/glove.42B.300d.txt'  # todo: change to your word2vec path
 
         if not os.path.exists('../tmp'):
             os.makedirs('../tmp')
